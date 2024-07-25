857ed636c79f6774cc713c5270f4106cf1807a33
NehalSurti <nehalsurti9998@gmail.com>
2024-07-26 00:38:03 +0100

    updating

--- controller/image.js
+++ controller/image.js
@@ -1,6 +1,8 @@
+const { ClarifaiStub, grpc } = require("clarifai-nodejs-grpc");
+const { UserIdSchema } = require("../validation/userValidation.js");
+const prisma = require("../DB/prismaClient.js");
 const dotenv = require("dotenv");
 dotenv.config();
-const { ClarifaiStub, grpc } = require("clarifai-nodejs-grpc");
 
 const stub = ClarifaiStub.grpc();
 
@@ -9,42 +11,69 @@ const metadata = new grpc.Metadata();
 metadata.set("authorization", "Key " + process.env.PAT);
 
 exports.handleApiCall = (req, res) => {
-  stub.PostModelOutputs(
-    {
-      user_app_id: {
-        user_id: process.env.USER_ID,
-        app_id: process.env.APP_ID,
+  try {
+    stub.PostModelOutputs(
+      {
+        user_app_id: {
+          user_id: process.env.USER_ID,
+          app_id: process.env.APP_ID,
+        },
+        model_id: process.env.MODEL_ID,
+        version_id: process.env.MODEL_VERSION_ID, // This is optional. Defaults to the latest model version
+        inputs: [
+          {
+            data: { image: { url: req.body.input, allow_duplicate_url: true } },
+          },
+        ],
       },
-      model_id: process.env.MODEL_ID,
-      version_id: process.env.MODEL_VERSION_ID, // This is optional. Defaults to the latest model version
-      inputs: [
-        { data: { image: { url: req.body.input, allow_duplicate_url: true } } },
-      ],
-    },
-    metadata,
-    (err, response) => {
-      if (err) {
-        throw new Error(err);
-      }
+      metadata,
+      (err, response) => {
+        if (err) {
+          throw new Error(err);
+        }
 
-      if (response.status.code !== 10000) {
-        throw new Error(
-          "Post model outputs failed, status: " + response.status.description
-        );
-      }
+        if (response.status.code !== 10000) {
+          throw new Error(
+            "Post model outputs failed, status: " + response.status.description
+          );
+        }
 
-      // Since we have one input, one output will exist here
-      const output = response.outputs[0];
+        // Since we have one input, one output will exist here
+        const output = response.outputs[0];
 
-      console.log("Predicted concepts:");
-      for (const concept of output.data.concepts) {
-        console.log(concept.name + " " + concept.value);
+        console.log("Predicted concepts:");
+        for (const concept of output.data.concepts) {
+          console.log(concept.name + " " + concept.value);
+        }
+        res.status(200).json(response);
       }
-      res.json(response);
-    }
-  );
+    );
+  } catch (error) {
+    console.error("Error : ", error);
+    return res.status(500).json({ message: "Server error.", status: false });
+  }
 };
 
-exports.handleImage = (req, res) => {
-  const { id } = req.body;
+exports.handleImage = async (req, res) => {
+  try {
+    const { id } = req.body;
+    await UserIdSchema.validate({ id: Number(id) }, { abortEarly: false });
+
+    const updatedUserEntries = await prisma.user.update({
+      where: {
+        id: Number(id),
+      },
+      data: {
+        entries: {
+          increment: 1,
+        },
+      },
+    });
+
+    const { entries, ...others } = updatedUserEntries;
+    return res.status(200).json({ entries, status: true });
+  } catch (error) {
+    console.error("Error : ", error);
+    return res.status(500).json({ message: "Server error.", status: false });
+  }
 };
