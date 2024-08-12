448f22cfc2967ecc0f86ff2fbb31143dea83fe79
NehalSurti <nehalsurti9998@gmail.com>
2024-08-12 21:20:33 +0100

    updating

--- index.js
+++ index.js
@@ -27,6 +27,6 @@ app.use("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
-app.listen(5000, () => {
+app.listen(process.env.PORT, () => {
   console.log("Server Started");
 });
